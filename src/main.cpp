ef31160052584c80b9586d5b7843f9a451c3927b
Hezekiah Dombach <hezekiahdombach@gmail.com>
2023-01-17 22:28:50 -0500

    set up image views

--- src/main.cpp
+++ src/main.cpp
@@ -73,6 +73,7 @@ class KaleidoscopeApplication {
 		bool checkDeviceExtensionSupport(VkPhysicalDevice device);
 		void createLogicalDevice();
 		void createSwapChain();
+		void createImageViews();
 
 		bool checkValidationLayerSupport();
 		void mainLoop();
@@ -107,6 +108,8 @@ class KaleidoscopeApplication {
 		std::vector<VkImage> swapChainImages;
 		VkFormat swapChainImageFormat;
 		VkExtent2D swapChainExtent;
+
+		std::vector<VkImageView> swapChainImageViews;
 
 		const std::vector<const char*> validationLayers = {
 			"VK_LAYER_KHRONOS_validation"
@@ -145,6 +148,7 @@ void KaleidoscopeApplication::initVulkan() {
 	pickPhysicalDevice();
 	createLogicalDevice();
 	createSwapChain();
+	createImageViews();
 }
 
 void KaleidoscopeApplication::createInstance() {
@@ -228,6 +232,10 @@ void KaleidoscopeApplication::mainLoop() {
 }
 
 void KaleidoscopeApplication::cleanup() {
+	for (auto imageView : swapChainImageViews) {
+		vkDestroyImageView(device, imageView, nullptr);
+	}
+
 	vkDestroySwapchainKHR(device, swapChain, nullptr);
 
 	vkDestroyDevice(device, nullptr);
@@ -446,6 +454,31 @@ void KaleidoscopeApplication::createSwapChain() {
 
 	swapChainImageFormat = surfaceFormat.format;
 	swapChainExtent = extent;
+}
+
+void KaleidoscopeApplication::createImageViews() {
+	swapChainImageViews.resize(swapChainImages.size());
+
+	for (size_t i = 0; i < swapChainImages.size(); i++) {
+		VkImageViewCreateInfo createInfo{};
+		createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+		createInfo.image = swapChainImages[i];
+		createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+		createInfo.format = swapChainImageFormat;
+		createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+		createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+		createInfo.subresourceRange.baseMipLevel = 0;
+		createInfo.subresourceRange.levelCount = 1;
+		createInfo.subresourceRange.baseArrayLayer = 0;
+		createInfo.subresourceRange.layerCount = 1;
+
+		if (vkCreateImageView(device, &createInfo, nullptr, &swapChainImageViews[i]) != VK_SUCCESS) {
+			throw std::runtime_error("failed to create image views!");
+		}
+	}
 }
 
 QueueFamilyIndices KaleidoscopeApplication::findQueueFamilies(VkPhysicalDevice device) {
