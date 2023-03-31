80845d53c195f54ed9df9470e4cab0fbbbce6aad
Hezekiah Dombach <hezekiahdombach@gmail.com>
2023-03-31 19:51:31 -0400

    got setup script working

--- setup.sh
+++ setup.sh
@@ -1,3 +1,3 @@
 mkdir build
-conan install . --output-folder=build --build=missing
-cmake --preset conan-release -B build -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -GNinja -DCMAKE_BUILD_TYPE=Debug
+conan install . --output-folder=build --build=missing -s build_type=Debug
+cmake -B build -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_EXPORT_COMPILE_COMMANDS=1 -G Ninja
