d6ecad530c47714d45b95472a3d32c62b9ddc4e0
Hezekiah Dombach <hezekiahdombach@gmail.com>
2023-06-15 14:19:48 -0400

    fixed TextureView crashing when hidden

--- src/ui/textureView.cpp
+++ src/ui/textureView.cpp
@@ -1,4 +1,6 @@
 #include "textureView.h"
+#include "format.h"
+#include "log.h"
 #include "texture.h"
 #include <glm/fwd.hpp>
 #include <imgui.h>
@@ -8,9 +10,11 @@ namespace ui {
 
 	void TextureView::show() {
 		auto widgetSize = ImGui::GetContentRegionAvail();
-		if (texture_.isResizable() && (widgetSize.x != size_.x || widgetSize.y != size_.y)) {
-			size_ = widgetSize;
-			texture_.resize(glm::ivec2{size_.x, size_.y});
+		if (widgetSize.x >= 0 && widgetSize.y >= 0) {
+			if (texture_.isResizable() && (widgetSize.x != size_.x || widgetSize.y != size_.y)) {
+				size_ = widgetSize;
+				texture_.resize(glm::ivec2{size_.x, size_.y});
+			}
 		}
 
 		ImGui::Image(texture_.getDescriptorSet(), widgetSize);
