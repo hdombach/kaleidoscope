7817d753c76bbcb32e556aedf22e2d722235e81f
Hezekiah Dombach <hezekiahdombach@gmail.com>
2024-07-19 22:35:50 -0400

    added static buffer

---
+++ src/vulkan/StaticBuffer.cpp
@@ -0,0 +1,103 @@
+#include "StaticBuffer.hpp"
+
+#include <vulkan/vulkan_core.h>
+
+#include "../util/result.hpp"
+
+namespace vulkan {
+	util::Result<StaticBuffer, KError> StaticBuffer::create(
+			void *data,
+			VkDeviceSize range)
+	{
+		auto result = StaticBuffer();
+
+		result._range = range;
+
+		VkBuffer staging_buffer;
+		VkDeviceMemory staging_buffer_memory;
+
+		Graphics::DEFAULT->create_buffer(
+				range, 
+				VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 
+				VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, 
+				staging_buffer, 
+				staging_buffer_memory);
+
+		void *mapped_data;
+		auto res = vkMapMemory(
+				Graphics::DEFAULT->device(), 
+				staging_buffer_memory, 
+				0, 
+				range,
+				0,
+				&mapped_data);
+		if (res != VK_SUCCESS) {
+			return {res};
+		}
+
+		memcpy(mapped_data, data, range);
+
+		vkUnmapMemory(Graphics::DEFAULT->device(), staging_buffer_memory);
+
+		Graphics::DEFAULT->create_buffer(
+				range, 
+				VK_BUFFER_USAGE_TRANSFER_DST_BIT
+				| VK_BUFFER_USAGE_VERTEX_BUFFER_BIT
+				| VK_BUFFER_USAGE_STORAGE_BUFFER_BIT,
+				VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
+				result._buffer,
+				result._buffer_memory);
+
+		Graphics::DEFAULT->copy_buffer(staging_buffer, result._buffer, range);
+
+		vkDestroyBuffer(Graphics::DEFAULT->device(), staging_buffer, nullptr);
+		vkFreeMemory(Graphics::DEFAULT->device(), staging_buffer_memory, nullptr);
+
+		return {std::move(result)};
+	}
+
+	StaticBuffer::StaticBuffer(StaticBuffer &&other) {
+		_buffer = other._buffer;
+		other._buffer = nullptr;
+
+		_buffer_memory = other._buffer_memory;
+		other._buffer_memory = nullptr;
+
+		_range = other._range;
+	}
+
+	StaticBuffer& StaticBuffer::operator=(StaticBuffer&& other) {
+		destroy();
+
+		_buffer = other._buffer;
+		other._buffer = nullptr;
+
+		_buffer_memory = other._buffer_memory;
+		other._buffer_memory = nullptr;
+
+		_range = other._range;
+
+		return *this;
+	}
+
+	StaticBuffer::StaticBuffer():
+		_buffer(nullptr),
+		_buffer_memory(nullptr),
+		_range(0)
+	{}
+
+	void StaticBuffer::destroy() {
+		if (_buffer) {
+			vkDestroyBuffer(Graphics::DEFAULT->device(), _buffer, nullptr);
+			_buffer = nullptr;
+		}
+		if (_buffer_memory) {
+			vkFreeMemory(Graphics::DEFAULT->device(), _buffer_memory, nullptr);
+			_buffer_memory = nullptr;
+		}
+	}
+
+	StaticBuffer::~StaticBuffer() {
+		destroy();
+	}
+}
---
+++ src/vulkan/StaticBuffer.hpp
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+#include <vulkan/vulkan_core.h>
+
+#include "graphics.hpp"
+#include "../util/result.hpp"
+#include "../util/errors.hpp"
+
+namespace vulkan {
+	class StaticBuffer {
+		public:
+			static util::Result<StaticBuffer, KError> create(void *data, VkDeviceSize range);
+
+			template<typename T>
+				static util::Result<StaticBuffer, KError> create(std::vector<T> &buf) {
+					return create(buf.data(), buf.size() * sizeof(T));
+				}
+
+			StaticBuffer(const StaticBuffer& other) = delete;
+			StaticBuffer(StaticBuffer &&other);
+			StaticBuffer& operator=(const StaticBuffer& other) = delete;
+			StaticBuffer& operator=(StaticBuffer&& other);
+
+			StaticBuffer();
+
+			void destroy();
+			~StaticBuffer();
+
+			VkBuffer buffer() { return _buffer; }
+			VkDeviceSize range() { return _range; }
+
+		private:
+			VkBuffer _buffer;
+			VkDeviceMemory _buffer_memory;
+			VkDeviceSize _range;
+	};
+}
