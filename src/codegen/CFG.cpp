eda4cbbacc62556e48d0cfc783e998dfc1b20c9a
Hezekiah Dombach <hezekiahdombach@gmail.com>
2024-12-28 19:35:51 -0500

    Fixed cfg alt and seq operations

--- src/codegen/CFG.cpp
+++ src/codegen/CFG.cpp
@@ -1,10 +1,65 @@
 #include "CFG.hpp"
 
-#include "tests/Test.hpp"
 #include <sstream>
+#include <string>
+
+#include "tests/Test.hpp"
 
 namespace cg {
 	CFG::CFG(): _type(Type::none) { }
+
+	CFG::CFG(CFG &&other) {
+		_children = std::move(other._children);
+
+		_ref = other._ref;
+		other._ref = nullptr;
+
+		_content = std::move(other._content);
+
+		_type = other._type;
+		other._type = Type::none;
+
+		_name = std::move(other._name);
+	}
+
+	CFG &CFG::operator=(CFG &&other) {
+		destroy();
+
+		_children = std::move(other._children);
+
+		_ref = other._ref;
+		other._ref = nullptr;
+
+		_content = std::move(other._content);
+
+		_type = other._type;
+		other._type = Type::none;
+
+		_name = std::move(other._name);
+
+		return *this;
+	}
+
+	void CFG::destroy() {
+		_children.clear();
+		_ref = nullptr;
+		_content.clear();
+		_type = Type::none;
+		_name.clear();
+	}
+
+	CFG CFG::dup() const {
+		CFG result;
+		for (auto &child : _children) {
+			result._children.push_back(child.dup());
+		}
+		result._ref = _ref;
+		result._content = _content;
+		result._type = _type;
+		result._name = _name;
+
+		return result;
+	}
 
 	CFG CFG::literal(const std::string &str) {
 		auto result = CFG();
@@ -15,11 +70,15 @@ namespace cg {
 		return result;
 	}
 
-	CFG CFG::seq(CFG const &lhs, CFG const &rhs) {
-		auto tlhs = lhs;
-		auto trhs = rhs;
-		return seq(std::move(tlhs), std::move(trhs));
+	CFG CFG::ref(CFG const &other) {
+		auto result = CFG();
+
+		result._ref = &other;
+		result._type = Type::reference;
+
+		return result;
 	}
+
 	CFG CFG::seq(CFG &&lhs, CFG &&rhs) {
 		auto result = CFG();
 
@@ -40,29 +99,25 @@ namespace cg {
 		result._type = Type::sequence;
 
 		if (lhs.type() == Type::sequence) {
-			result._children = std::move(lhs.children());
+			result._children = std::move(lhs._children);
 		} else {
 			result._children.push_back(std::move(lhs));
 		}
 
 		if (rhs.type() == Type::sequence) {
-			result._children.insert(
-				result._children.end(),
-				lhs.children().begin(),
-				lhs.children().end()
-			);
+			for (auto &child : rhs._children) {
+				result._children.push_back(std::move(child));
+			}
 		} else {
 			result._children.push_back(std::move(rhs));
 		}
 
+		lhs.destroy();
+		rhs.destroy();
+
 		return result;
 	}
 
-	CFG CFG::alt(CFG const &lhs, CFG const &rhs) {
-		auto tlhs = lhs;
-		auto trhs = rhs;
-		return alt(std::move(tlhs), std::move(trhs));
-	}
 	CFG CFG::alt(CFG &&lhs, CFG &&rhs) {
 		auto result = CFG();
 
@@ -76,17 +131,15 @@ namespace cg {
 		}
 
 		if (lhs.type() == Type::alternative) {
-			result._children = std::move(lhs.children());
+			result._children = std::move(lhs._children);
 		} else {
 			result._children.push_back(std::move(lhs));
 		}
 
 		if (rhs.type() == Type::alternative) {
-			result._children.insert(
-				result._children.end(),
-				rhs.children().begin(),
-				rhs.children().end()
-			);
+			for (auto &child : rhs._children) {
+				result._children.push_back(std::move(child));
+			}
 		} else {
 			result._children.push_back(std::move(rhs));
 		}
@@ -103,6 +156,9 @@ namespace cg {
 	CFG::Type CFG::type() const {
 		return _type;
 	}
+	void CFG::set_name(std::string const &name) {
+		_name = name;
+	}
 
 	std::ostream &CFG::debug(std::ostream &os) const {
 		bool first = true;
@@ -111,12 +167,18 @@ namespace cg {
 				return os;
 			case Type::literal:
 				return os << "\"" << _content << "\"";
+			case Type::reference:
+				if (_ref->_name.empty()) {
+					return os << "<ref>";
+				} else {
+					return os << _ref->_name;
+				}
 			case Type::sequence:
 				for (auto &child : _children) {
 					if (first) {
 						first = false;
 					} else {
-						os << "+ ";
+						os << " + ";
 					}
 
 					if (child.type() == Type::alternative) {
@@ -126,9 +188,8 @@ namespace cg {
 					if (child.type() == Type::alternative) {
 						os << ")";
 					}
-
-					return os;
 				}
+				return os;
 			case Type::alternative:
 				for (auto &child : _children) {
 					if (first) {
@@ -139,9 +200,8 @@ namespace cg {
 
 					os << child;
 				}
+				return os;
 		}
-
-		return os;
 	}
 
 	std::string CFG::str() const {
@@ -151,47 +211,132 @@ namespace cg {
 	}
 
 	TEST(cfg, literal) {
-		auto literal_cfg = CFG("simpler cfg");
+		auto literal_cfg = CFG::literal("simpler cfg");
 		EXPECT_EQ(literal_cfg.str(), "\"simpler cfg\"");
 
-		CFG literal_cfg2 = "another simpler cfg";
+		CFG literal_cfg2 = "another simpler cfg"_cfg;
 		EXPECT_EQ(literal_cfg2.str(), "\"another simpler cfg\"");
 	}
 
+	TEST(cfg, ref) {
+		auto anon_value = "anon"_cfg;
+
+		auto named_value = "named"_cfg;
+		named_value.set_name("named_ref");
+
+		auto combined = anon_value + named_value;
+		EXPECT_EQ(combined.str(), "<ref> + named_ref");
+	}
+
 	TEST(cfg, concat_str) {
-		auto first = CFG() + "f" + "ir" + "st";
+		auto first = "f"_cfg + "ir"_cfg + "st"_cfg;
 		EXPECT_EQ(first.str(), "\"first\"");
 
-		auto second = CFG() + "second" + CFG();
+		auto second = CFG() + CFG::literal("second") + CFG();
 		EXPECT_EQ(second.str(), "\"second\"");
 
-		auto firstsecond = first + second + CFG();
-		EXPECT_EQ(firstsecond.str(), "\"firstsecond\"");
+		auto first_sec = first.dup() + second.dup();
+		EXPECT_EQ(first_sec.str(), "\"firstsecond\"");
+
+		auto first_third = CFG::seq(first.dup(), "third"_cfg);
+		EXPECT_EQ(first_third.str(), "\"firstthird\"");
+	}
+
+	TEST(cfg, concat) {
+		auto first = "first"_cfg;
+		first.set_name("first_ref");
+
+		auto second = "second"_cfg;
+		second.set_name("second_ref");
+
+		auto third = "third"_cfg;
+		third.set_name("third_ref");
+
+		auto first_sec = first + second;
+		EXPECT_EQ(first_sec.str(), "first_ref + second_ref");
+
+		auto first_sec2 = first + second.dup();
+		EXPECT_EQ(first_sec2.str(), "first_ref + \"second\"");
+
+		auto first_sec3 = first.dup() + second;
+		EXPECT_EQ(first_sec3.str(), "\"first\" + second_ref");
+
+		auto first_sec_third = first + second + third;
+		EXPECT_EQ(first_sec_third.str(), "first_ref + second_ref + third_ref");
+
+		auto first_sec_third2 = first + second.dup() + third;
+		EXPECT_EQ(first_sec_third2.str(), "first_ref + \"second\" + third_ref");
+
+		auto first_sec_third3 = first.dup() + "_"_cfg + "_"_cfg + third;
+		EXPECT_EQ(first_sec_third3.str(), "\"first__\" + third_ref");
+
 	}
 
 	TEST(cfg, alt) {
-		auto first = CFG("first");
-		auto second = CFG("second");
+		auto first = "first"_cfg;
+		first.set_name("first_ref");
 
-		auto first_second = first | second;
-		EXPECT_EQ(first_second.str(), "\"first\" | \"second\"");
+		auto second = "second"_cfg;
+		second.set_name("second_ref");
 
-		auto first_second_third = first | second | "third";
-		EXPECT_EQ(first_second_third.str(), "\"first\" | \"second\" | \"third\"");
+		auto third = "third"_cfg;
+		third.set_name("third_ref");
 
-		auto first_second_third2 = first | (second | "third2");
-		EXPECT_EQ(first_second_third2.str(), "\"first\" | \"second\" | \"third2\"");
+		auto first_sec = first | second;
+		EXPECT_EQ(first_sec.str(), "first_ref | second_ref");
 
-		auto first_second_third3 = (first | second) | "third3";
-		EXPECT_EQ(first_second_third3.str(), "\"first\" | \"second\" | \"third3\"");
+		auto first_sec2 = first.dup() | second;
+		EXPECT_EQ(first_sec2.str(), "\"first\" | second_ref");
+
+		auto first_sec3 = first | second.dup();
+		EXPECT_EQ(first_sec3.str(), "first_ref | \"second\"");
+
+		auto first_sec4 = first.dup() | second.dup();
+		EXPECT_EQ(first_sec4.str(), "\"first\" | \"second\"");
+
+		auto first_sec_third = first | second | third;
+		EXPECT_EQ(first_sec_third.str(), "first_ref | second_ref | third_ref");
+
+		auto first_sec_third2 = (first | second) | third;
+		EXPECT_EQ(first_sec_third2.str(), "first_ref | second_ref | third_ref");
+
+		auto first_sec_third3 = first | (second | third);
+		EXPECT_EQ(first_sec_third3.str(), "first_ref | second_ref | third_ref");
+
+		auto first_sec_third4 = (first | second.dup()) | third;
+		EXPECT_EQ(first_sec_third4.str(), "first_ref | \"second\" | third_ref");
 	}
 
 	TEST(cfg, alt_concat) {
-		auto first = CFG("first");
-		auto second = CFG("second");
-		auto third = CFG("third");
+		auto first = "first"_cfg;
+		first.set_name("first_ref");
+		auto second = "second"_cfg;
+		second.set_name("second_ref");
+		auto third = "third"_cfg;
+		third.set_name("third_ref");
+		auto fourth = "fourth"_cfg;
+		fourth.set_name("fourth_ref");
 
 		auto test1 = first + second | third;
+		EXPECT_EQ(test1.str(), "first_ref + second_ref | third_ref");
+
+		auto test2 = (first + second) | third;
+		EXPECT_EQ(test2.str(), "first_ref + second_ref | third_ref");
+
+		auto test3 = first + (second | third);
+		EXPECT_EQ(test3.str(), "first_ref + (second_ref | third_ref)");
+
+		auto test4 = first.dup() + (second.dup() | third);
+		EXPECT_EQ(test4.str(), "\"first\" + (\"second\" | third_ref)");
+
+		auto test5 = first.dup() + second.dup() | third;
+		EXPECT_EQ(test5.str(), "\"firstsecond\" | third_ref");
+
+		auto test_long = (first + second) | (third + fourth);
+		EXPECT_EQ(test_long.str(), "first_ref + second_ref | third_ref + fourth_ref");
+
+		auto test_long2 = first + (second | third) + fourth;
+		EXPECT_EQ(test_long2.str(), "first_ref + (second_ref | third_ref) + fourth_ref");
 	}
 }
 
--- src/codegen/CFG.hpp
+++ src/codegen/CFG.hpp
@@ -10,6 +10,7 @@ namespace cg {
 			enum class Type {
 				none,
 				literal,
+				reference,
 				sequence,
 				alternative,
 			};
@@ -19,19 +20,28 @@ namespace cg {
 		public:
 			CFG();
 
-			CFG(std::string const &str) { *this = CFG::literal(str); }
-			CFG(const char *str) { *this = CFG::literal(str); }
+			CFG(const CFG &other) = delete;
+			CFG(CFG &&other);
+			CFG& operator=(const CFG &other) = delete;
+			CFG & operator=(CFG&& other);
+
+			void destroy();
+			~CFG() { destroy(); }
+
+			CFG dup() const;
+
 			static CFG literal(std::string const &str);
 
-			static CFG seq(CFG const &lhs, CFG const &rhs);
+			static CFG ref(CFG const &other);
+
 			static CFG seq(CFG &&lhs, CFG &&rhs);
 
-			static CFG alt(CFG const &lhs, CFG const &rhs);
 			static CFG alt(CFG &&lhs, CFG &&rhs);
 
 			Container const &children() const;
 			std::string const &content() const;
 			Type type() const;
+			void set_name(std::string const &str);
 
 			std::ostream &debug(std::ostream &os) const;
 
@@ -40,22 +50,40 @@ namespace cg {
 
 		private:
 			Container _children;
+			CFG const *_ref = nullptr;
 			std::string _content;
 			Type _type;
+			std::string _name;
 	};
 
+	inline CFG operator ""_cfg(const char * str, size_t) {
+		return CFG::literal(str);
+	}
+
 	inline CFG operator+(CFG const &lhs, CFG const &rhs) {
-		return CFG::seq(lhs, rhs);
+		return CFG::seq(CFG::ref(lhs), CFG::ref(rhs));
 	};
 	inline CFG operator+(CFG &&lhs, CFG &&rhs) {
-		return CFG::seq(lhs, rhs);
+		return CFG::seq(std::move(lhs), std::move(rhs));
+	}
+	inline CFG operator+(CFG const &lhs, CFG &&rhs) {
+		return CFG::seq(CFG::ref(lhs), std::move(rhs));
+	}
+	inline CFG operator+(CFG &&lhs, CFG const &rhs) {
+		return CFG::seq(std::move(lhs), CFG::ref(rhs).dup());
 	}
 
 	inline CFG operator|(CFG const &lhs, CFG const &rhs) {
-		return CFG::alt(lhs, rhs);
+		return CFG::alt(CFG::ref(lhs), CFG::ref(rhs));
 	}
 	inline CFG operator|(CFG &&lhs, CFG &&rhs) {
-		return CFG::alt(lhs, rhs);
+		return CFG::alt(std::move(lhs), std::move(rhs));
+	}
+	inline CFG operator|(CFG const &lhs, CFG &&rhs) {
+		return CFG::alt(CFG::ref(lhs), std::move(rhs));
+	}
+	inline CFG operator|(CFG &&lhs, CFG const &rhs) {
+		return CFG::alt(std::move(lhs), CFG::ref(rhs));
 	}
 }
 
