d38551796d0845b8ae723abab47396367881bd46
Hezekiah Dombach <hezekiahdombach@gmail.com>
2026-04-03 13:13:32 -0400

    Added documentation

--- src/codegen/CfgNode.hpp
+++ src/codegen/CfgNode.hpp
@@ -45,9 +45,20 @@ namespace cg {
 			Type type() const { return _type; }
 
 			Token::Type token_type() const { return _token_type; }
+			/**
+			 * @brief Get the name of the variable referenced
+			 *
+			 * Only valid if the token type is var
+			 */
 			std::string const &var_name() const { return _var_name; }
 
+			/**
+			 * @brief Prints a debug representation of the leaf to an ostream
+			 */
 			std::ostream& print_debug(std::ostream &os) const;
+			/**
+			 * @breif Gets a debug representation of the leaf
+			 */
 			std::string str() const;
 
 			bool operator==(CfgLeaf const &other) const;
@@ -60,32 +71,66 @@ namespace cg {
 
 	/**
 	 * @brief Descibes a single replacement rule for the context free grammar
+	 *
+	 * Consists of a sequence of leaf nodes which must be matched against
 	 */
 	class CfgRule final {
 		public:
+			/**
+			 * @brief Creates an empty rule set
+			 */
 			CfgRule() = default;
+			/**
+			 * @brief Creates a rule set containing a single leaf node to match against
+			 */
 			CfgRule(CfgLeaf const &leaf);
+			/**
+			 * @brief Creates a CfgRule from a concatenation of two other rules
+			 */
 			CfgRule(CfgRule const &lhs, CfgRule const &rhs);
+			/**
+			 * @brief Creates a CfgRule directly from a list of leaves
+			 */
 			CfgRule(std::vector<CfgLeaf> const &leaves);
+			/**
+			 * @brief Creates a CfgRule directly from a list of tokens
+			 */
 			CfgRule(std::vector<Token::Type> const &tokens);
 
 			std::vector<CfgLeaf> const &leaves() const { return _leaves; }
 
+			/**
+			 * @brief Gets the uid
+			 */
 			uint32_t set_id() const;
+			/**
+			 * @brief Sets the uid
+			 *
+			 * Should only be set by the CfgContext
+			 */
 			void set_set_id(uint32_t id);
 
 
+			/**
+			 * @brief Prints the debug representation of the rule to an ostream
+			 */
 			std::ostream& print_debug(std::ostream &os) const;
+			/**
+			 * @brief Gets the debug representation of the rule
+			 */
 			std::string str() const;
 			bool operator==(CfgRule const &other) const;
 			bool operator!=(CfgRule const &other) const;
 		private:
 			std::vector<CfgLeaf> _leaves;
-			uint32_t _set_id;
+			uint32_t _set_id = 0;
 	};
 
 	/**
-	 * @brief A collection of alternal rules
+	 * @brief A collection of alternative rules
+	 *
+	 * Consists of a sequence of CfgRules. Only one of them must be matched.
+	 * The rules are tested in the order they are added until the first one matches
 	 */
 	class CfgRuleSet final {
 		public:
@@ -93,23 +138,71 @@ namespace cg {
 			using iterator = RuleContainer::iterator;
 			using const_iterator = RuleContainer::const_iterator;
 
+			/**
+			 * @brief Creates an empty rule set
+			 */
 			CfgRuleSet() = default;
 
+			/**
+			 * @brief Creates an empty rule set with a name
+			 *
+			 * The name can be referenced by a leaf var name
+			 */
 			CfgRuleSet(std::string const &name);
+			/**
+			 * @brief Creates a rule set with a name and list of rules
+			 *
+			 * The name can be referenced by a leaf var name
+			 * The rule alternatives to match against
+			 */
 			CfgRuleSet(std::string const &name, std::vector<CfgRule> &&rules);
 
+			/**
+			 * @brief Sets the list of rules to the contents of another rule set
+			 *
+			 * It does not function like a normal assign operation in that the rule
+			 * set name remains the same.
+			 */
 			CfgRuleSet& operator=(CfgRuleSet const &set);
+			/**
+			 * @brief Sets the list of rules to a single rule
+			 *
+			 * It does not function like a normal assign operation in that the rule
+			 * set name remains the same.
+			 */
 			CfgRuleSet& operator=(CfgRule const &rule);
+			/**
+			 * @brief Sets the list of rules to a single rule constructed from a leaf
+			 *
+			 * It does not function like a normal assign operation in that the rule
+			 * set name remains the same
+			 */
 			CfgRuleSet& operator=(CfgLeaf const &leaf);
 
+			/**
+			 * @brief Appends a single rule to test against
+			 */
 			void add_rule(CfgRule const &rule);
+			/**
+			 * @brief Appends a list of rules to test against
+			 */
 			void add_rules(CfgRuleSet const &set);
 
+			/**
+			 * @brief Sets the rule set name
+			 *
+			 * The name can be reference by a leaf var name
+			 */
 			void set_name(std::string const &name) { _name = name; }
 			std::string const &name() const { return _name; }
 			std::vector<CfgRule> &rules() { return _rules; }
 			std::vector<CfgRule> const &rules() const { return _rules; }
 
+			/**
+			 * @brief Updates the list of rules to be matched against
+			 *
+			 * The rules tested against in order
+			 */
 			void set_rules(std::vector<CfgRule> &&rules);
 
 			iterator begin();
@@ -117,7 +210,13 @@ namespace cg {
 			iterator end();
 			const_iterator end() const;
 
+			/**
+			 * @brief Prints a debug representation of the rule set to an ostream
+			 */
 			std::ostream& print_debug(std::ostream &os, bool multiline=false) const;
+			/**
+			 * @brief Gets a debug representaiton of the rule set
+			 */
 			std::string str(bool multiline=false) const;
 		private:
 			std::string _name;
@@ -125,6 +224,7 @@ namespace cg {
 	};
 
 	/******** CfgRule *********/
+
 	inline CfgRule operator + (Token::Type const &lhs, Token::Type const &rhs) {
 		return CfgRule(CfgLeaf(lhs), CfgLeaf(rhs));
 	}
@@ -146,7 +246,7 @@ namespace cg {
 	}
 
 
-	/*******8 Rule set ********/
+	/******** Rule set ********/
 
 	inline CfgRuleSet operator | (Token::Type const &lhs, Token::Type const &rhs) {
 		auto set = CfgRuleSet();
